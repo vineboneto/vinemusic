5145139ebd76e76804d439b44298955927105f8c
vineboneto <vineboneto@gmail.com>
2024-09-29 03:00:10 -0300

    feat: header

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,15 +2,21 @@ import { Stack } from "expo-router";
 import { useFonts } from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
 import { useEffect } from "react";
+import { type StyleProp, Text, type ViewStyle } from "react-native";
+import { Font } from "@/constants/Font";
 
 SplashScreen.preventAutoHideAsync();
 
+const defaultStyle: StyleProp<ViewStyle> = {
+	backgroundColor: "#fff",
+};
+
 export default function RootLayout() {
 	const [loaded, error] = useFonts({
-		"Jungle-Regular": require("../assets/fonts/Junge-Regular.ttf"),
-		"Inter_28pt-Medium": require("../assets/fonts/Inter_28pt-Medium.ttf"),
-		"Inter_28pt-Regular": require("../assets/fonts/Inter_28pt-Regular.ttf"),
-		"Inter_28pt-SemiBold": require("../assets/fonts/Inter_28pt-SemiBold.ttf"),
+		[Font.JungleRegular]: require("../assets/fonts/Junge-Regular.ttf"),
+		[Font.InterMedium]: require("../assets/fonts/Inter_28pt-Medium.ttf"),
+		[Font.InterRegular]: require("../assets/fonts/Inter_28pt-Regular.ttf"),
+		[Font.InterSemiBold]: require("../assets/fonts/Inter_28pt-SemiBold.ttf"),
 	});
 
 	useEffect(() => {
@@ -24,8 +30,21 @@ export default function RootLayout() {
 	}
 
 	return (
-		<Stack>
-			<Stack.Screen name="index" />
+		<Stack
+			screenOptions={{
+				headerTitleAlign: "center",
+				headerShadowVisible: false,
+				headerTitle: (props) => (
+					<Text
+						{...props}
+						style={{ fontSize: 24, fontFamily: "Jungle-Regular" }}
+					>
+						Vine Music
+					</Text>
+				),
+			}}
+		>
+			<Stack.Screen name="index" options={{ contentStyle: defaultStyle }} />
 		</Stack>
 	);
 }
---
+++ constants/Font.ts
@@ -0,0 +1,6 @@
+export const Font = {
+	JungleRegular: "Jungle-Regular",
+	InterMedium: "Inter_28pt-Medium",
+	InterRegular: "Inter_28pt-Regular",
+	InterSemiBold: "Inter_28pt-SemiBold",
+};
