1b3fec0735d61ebf98c079b661793f7860e1a1ce
vineboneto <vineboneto@gmail.com>
2024-09-29 22:20:01 -0300

    feat: add flat list

--- app/(tabs)/home/index.tsx
+++ app/(tabs)/home/index.tsx
@@ -1,7 +1,7 @@
 import { ButtonActionsGroup } from "@/components/home/button-action";
 import { Card } from "@/components/home/card";
 import { Font } from "@/constants/Font";
-import { Text, View } from "react-native";
+import { FlatList, SafeAreaView, Text, View } from "react-native";
 
 function Title() {
 	return (
@@ -11,13 +11,44 @@ function Title() {
 	);
 }
 
+type MusicItemData = {
+	status: "finish" | "pendent";
+	title: string;
+	timeInMinutes: number;
+};
+
+const data: MusicItemData[] = [
+	{ status: "finish", title: "Piano", timeInMinutes: 30 },
+	{ status: "pendent", title: "Guitarra", timeInMinutes: 60 * 4 },
+];
+
 export default function Index() {
 	return (
-		<View style={{ flex: 1, marginHorizontal: 20, rowGap: 20, marginTop: 20 }}>
+		<View
+			style={{
+				flex: 1,
+				marginHorizontal: 20,
+				rowGap: 20,
+				marginTop: 20,
+				marginBottom: 115,
+			}}
+		>
 			<ButtonActionsGroup />
 			<Title />
-			<Card status="finish" title="Piano" timeInMinutes={30} />
-			<Card status="pendent" title="Guitarra" timeInMinutes={60 * 4} />
+			<SafeAreaView>
+				<FlatList
+					data={new Array(20).fill(0).map((_, idx) => data[idx % 2])}
+					renderItem={({ item }) => (
+						<Card
+							title={item.title}
+							timeInMinutes={item.timeInMinutes}
+							status={item.status}
+						/>
+					)}
+					keyExtractor={(item, idx) => idx.toString()}
+					ItemSeparatorComponent={() => <View style={{ height: 10 }} />}
+				/>
+			</SafeAreaView>
 		</View>
 	);
 }
